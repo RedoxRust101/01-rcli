964eb43af277128fad423f27acc0de1d3c71c274
cm <cm@k640.workgroup>
2024-05-15 14:21:23 +0800

    feature: support base64 encode/decode cli

--- src/cli/csv.rs
+++ src/cli/csv.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-use std::{fmt, path::Path, str::FromStr};
+use std::{fmt, str::FromStr};
+
+use super::verify_input_file;
 
 #[derive(Debug, Clone, Copy)]
 pub enum OutputFormat {
@@ -7,20 +9,6 @@ pub enum OutputFormat {
     Yaml,
 }
 
-#[derive(Debug, Parser)]
-#[command(name = "rcli", version, author, about, long_about = None)]
-pub struct Opts {
-    #[command(subcommand)]
-    pub cmd: SubCommand,
-}
-
-#[derive(Debug, Parser)]
-pub enum SubCommand {
-    #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
-    Csv(CsvOpts),
-    #[command(name = "genpass", about = "Generate random password")]
-    GenPass(GenPassOpts),
-}
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser=verify_input_file)]
@@ -38,28 +26,7 @@ pub struct CsvOpts {
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
-#[derive(Debug, Parser)]
-pub struct GenPassOpts {
-    #[arg(short, long, default_value_t = 16)]
-    pub length: u8,
-    #[arg(long, default_value_t = true)]
-    pub uppercase: bool,
-    #[arg(long, default_value_t = true)]
-    pub lowercase: bool,
-    #[arg(long, default_value_t = true)]
-    pub number: bool,
-    #[arg(long, default_value_t = true)]
-    pub symbol: bool,
-}
-fn verify_input_file(filename: &str) -> Result<String, String> {
-    if !filename.ends_with(".csv") {
-        return Err("Input file must be a CSV file".into());
-    }
-    if !Path::new(filename).exists() {
-        return Err("Input file not exists".into());
-    }
-    Ok(filename.into())
-}
+
 fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
     format.parse::<OutputFormat>()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod opts;
+mod cli;
 mod process;
-pub use opts::{GenPassOpts, Opts, SubCommand};
-pub use process::{process_csv, process_genpass};
+pub use cli::{Base64Format, Base64SubCommand, Opts, SubCommand};
+pub use process::*;
